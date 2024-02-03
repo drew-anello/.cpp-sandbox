5aeea306047c4e4e364a67acb8a3282edf3c7ed3
Drew Anello <drew.anello@gmail.com>
2024-02-02 21:24:32 -0500

    complies without errors

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+  int favoriteNumber;
+
+  std::cout << "Enter your favorite Number between 1 and 100:";
+
+  std::cin >> favoriteNumber;
+
+  std::cout << "That's my favorite number too!" << std::endl;
+
+  return 0;
+}
